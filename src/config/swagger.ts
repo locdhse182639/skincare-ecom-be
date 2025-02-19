c261f595e2359b1a038e86e9725cd32dd24feb01
locdhse182639 <shigesotarks@gmail.com>
2025-02-19 15:20:07 +0700

    Initial backend setup

---
+++ src/config/swagger.ts
@@ -0,0 +1,22 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Skincare E-Commerce API",
+      version: "1.0.0",
+      description: "API documentation for the Skincare E-Commerce backend",
+    },
+    servers: [{ url: "http://localhost:5000" }],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+export const setupSwagger = (app: Express) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
