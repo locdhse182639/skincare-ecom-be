67bd4b74b255662025cbfd2c5cfae7cd7a67eb62
locdhse182639 <shigesotarks@gmail.com>
2025-02-19 22:17:27 +0700

    04-user-profile-apis

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { setupSwagger } from "./config/swagger";
 import authRoutes from "./routes/auth.routes";
+import userRoutes from "./routes/user.routes";
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(helmet());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Skincare E-Commerce Backend is Running!");
